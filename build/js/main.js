c8edd0f691d7764dd29ba8ab891c13d030601d57
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-06-01 08:12:05 -0300

    Class 3 - Basic TS Types

--- build/js/main.js
+++ build/js/main.js
@@ -1,14 +1,21 @@
 "use strict";
-let username = "Felipe Guerra";
-console.log(username);
-// // It will compile the code to JS even though id does not like it
-// let a = 12
-// let b = '6'
-// let c = 2
-// instead we cvan do this:
-let a = 12;
-let b = 6;
-let c = 2;
-// TS will compile, however it does not liek it
-console.log(a / b);
-console.log(c * b);
+let myName = 'Dave';
+let meaningOfLife;
+let isLoading;
+// It let all types to be assigned to the any type
+let album;
+// // Instead we can do the union type not limited to 2 data types
+// let album: string | number
+myName = 'jonh';
+meaningOfLife = 42;
+isLoading = true;
+album = 1924;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+// In the case of a program were you use a variable you cannot figure out the type.
+// As the RegExp, you insert it and then jsut see the suggested type in TS
+// let re = /\w+/g
+let re = /\w+/g;
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,25 @@
-let username = "Felipe Guerra"
-console.log(username)
+let myName: string = 'Dave'
+let meaningOfLife: number
+let isLoading: boolean
+// It let all types to be assigned to the any type
+let album: any
 
-// // It will compile the code to JS even though id does not like it
-// let a = 12
-// let b = '6'
-// let c = 2
+// // Instead we can do the union type not limited to 2 data types
+// let album: string | number
 
-// instead we cvan do this:
-let a: number = 12
-let b: number = 6
-let c: number = 2
+myName = 'jonh'
+meaningOfLife = 42
+isLoading = true
+album = 1924
 
+const sum = (a:  number, b: number ) => { 
+    return a + b
+ }
 
+let postId: string | number
+let isActive: number | boolean
 
-// TS will compile, however it does not liek it
-console.log(a / b)
-
-console.log(c * b)
-
-
+// In the case of a program were you use a variable you cannot figure out the type.
+// As the RegExp, you insert it and then jsut see the suggested type in TS
+// let re = /\w+/g
+let re: RegExp = /\w+/g
