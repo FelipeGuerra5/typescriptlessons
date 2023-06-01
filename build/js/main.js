8a691400ab90d4f0951ed86daccf305c6c7641dc
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-06-01 07:46:21 -0300

    Class 1 is done

---
+++ build/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+let username = "Felipe Guerra";
+console.log(username);
+// // It will compile the code to JS even though id does not like it
+// let a = 12
+// let b = '6'
+// let c = 2
+// instead we cvan do this:
+let a = 12;
+let b = 6;
+let c = 2;
+// TS will compile, however it does not liek it
+console.log(a / b);
+console.log(c * b);
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+let username = "Felipe Guerra"
+console.log(username)
+
+// // It will compile the code to JS even though id does not like it
+// let a = 12
+// let b = '6'
+// let c = 2
+
+// instead we cvan do this:
+let a: number = 12
+let b: number = 6
+let c: number = 2
+
+
+
+// TS will compile, however it does not liek it
+console.log(a / b)
+
+console.log(c * b)
+
+
