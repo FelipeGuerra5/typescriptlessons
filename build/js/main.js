fecf5bbd943e9114f46e742caf9357758b860d3e
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-06-14 14:19:55 -0300

    Class 8 - Generic Types

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,122 @@
 "use strict";
+const stringEcho = (arg) => arg; // Only works with a string type
+const Echo = (arg) => arg; // Works with any type
+const isObj = (arg) => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null);
+};
+console.log(isObj(true));
+console.log(isObj('Jonh'));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: 'Jonh' }));
+console.log(isObj(null));
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg }; // !! transforms 0 and 1 to a Boolean
+};
+console.log('Second batch');
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue('Dave'));
+console.log(isTrue(''));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue([])); // Modified
+console.log(isTrue({ name: 'Dave' }));
+console.log(isTrue([])); //Modified
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
+const chockBoolValue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { value: arg, is: false };
+    }
+    return { value: arg, is: !!arg }; // !! transforms 0 and 1 to a Boolean
+};
+const processUser = (user) => {
+    // Process the user with logic here
+    return user;
+};
+console.log(processUser({ id: 1, name: 'Dave' }));
+// console.log(processUser({ name:'Dave'})) // The TS does not like it, for ID is necessary
+const getUsersProperty = (users, key) => {
+    return users.map(user => user[key]);
+};
+const usersArray = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+];
+console.log(getUsersProperty(usersArray, "email"));
+console.log(getUsersProperty(usersArray, "username"));
+console.log(getUsersProperty(usersArray, "company"));
+class StateObject {
+    constructor(value) {
+        this.data = value;
+    }
+    get state() {
+        return this.data;
+    }
+    set state(value) {
+        this.data = value;
+    }
+}
+const store = new StateObject("Jonh");
+console.log(store.state);
+store.state = 'Dave';
+// store.state = 12 // After the data is passed the TS undestands it as being the norm
+const myState = new StateObject([15]);
+myState.state = (['Dave, 42, true']);
+console.log(myState.state);
